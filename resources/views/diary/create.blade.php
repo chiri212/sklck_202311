e8df495fa5108d05bf884e234f7d023aea9f65b4
genichiorita <chiri212@gmail.com>
2023-11-11 23:38:07 +0900

    新規登録・編集でのバリデーションエラー時に元の入力値をフォームに保持する修正

--- resources/views/diary/create.blade.php
+++ resources/views/diary/create.blade.php
@@ -6,12 +6,12 @@
     <form action="{{route('diary.store')}}" method="post" class="">
     @csrf
     <div class="mb-3">
-        <label for="text" class="form-label">投稿内容
+        <label for="text" class="form-label">投稿内容 <span class="text-danger">*</span>
             @error('text')
             <span class="fs-6 text-danger"> {{$message}}</span>
             @enderror
         </label>
-        <input type="text" class="form-control" id="text" name="text" placeholder="投稿内容を記入">
+        <input type="text" class="form-control" id="text" name="text" value="{{old('text')}}" placeholder="投稿内容を記入">
     </div>
     <div class="mb-3 d-flex justify-content-end">
         <button type="submit" class="btn btn-primary px-4">投稿</button>
--- resources/views/diary/edit.blade.php
+++ resources/views/diary/edit.blade.php
@@ -7,12 +7,12 @@
     @method('PUT')
     @csrf
     <div class="mb-3">
-        <label for="text" class="form-label">投稿内容
+        <label for="text" class="form-label">投稿内容 <span class="text-danger">*</span>
             @error('text')
             <span class="fs-6 text-danger"> {{$message}}</span>
             @enderror
         </label>
-        <input type="text" class="form-control" id="text" name="text" value="{{$diary->text}}" placeholder="投稿内容を記入">
+        <input type="text" class="form-control" id="text" name="text" value="{{old('text', $diary->text)}}" placeholder="投稿内容を記入">
     </div>
     <div class="mb-3 d-flex justify-content-end">
         <button type="submit" class="btn btn-primary px-4">更新</button>
